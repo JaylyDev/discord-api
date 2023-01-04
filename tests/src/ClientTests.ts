96c025800ed721d90b8e8a93c8bfad6e7e4f0f27
bigguy69420 <65847850+JaylyDev@users.noreply.github.com>
2023-01-04 15:52:49 +0800

    Update ClientTests.ts

--- tests/src/ClientTests.ts
+++ tests/src/ClientTests.ts
@@ -1,7 +1,6 @@
 import * as GameTest from "@minecraft/server-gametest";
-import type { ChannelType } from "discord-api-types/v9";
 import { client, TEST_CHANNEL, TEST_GUILD } from "./config";
-import { GuildTextChannel, Message } from './discord-api';
+import { ChannelType, Message } from './discord-api';
 
 GameTest.register("ClientTests", "get_guild", async (test) => {
   const guild = await client.guild.get(TEST_GUILD);
@@ -12,7 +11,7 @@ GameTest.register("ClientTests", "get_guild", async (test) => {
   .structureName('discord_gametest');
 
 GameTest.register("ClientTests", "send_message", async (test) => {
-  const channel = await client.channel.get(TEST_CHANNEL) as GuildTextChannel<ChannelType.GuildText>;
+  const channel = await client.channel.get(TEST_CHANNEL, ChannelType.GuildText);
   const message = await channel.sendMessage({
     content: "Hello World",
     embeds: [{
